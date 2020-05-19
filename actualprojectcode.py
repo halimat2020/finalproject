1c505538e19669e62b40ed12d7e2171a303cc57e
Halimat Farayola <63603204+halimat2020@users.noreply.github.com>
2020-05-18 23:17:00 -0400

    Update actualprojectcode.py

--- actualprojectcode.py
+++ actualprojectcode.py
@@ -1,62 +1,52 @@
- # Tk label demo: Minimal demo
+ Tk label demo: Minimal demo
+ #Designed audio filters on python and created a program that 
+ #allows users to select the filters from a GUI and processes their audio input in real time
 import Tkinter as Tk   	# for Python 3
 import pyaudio
 from myfunctions import clip16
-
 import struct
 import math
+from scipy import signal
 import numpy as np
+#code for filter definition
+
 def funN1(input_tuple):
-	import stftfun
-	import istftfun
-	
-	R = 512
-	Nfft = 512
-	N=len(input_tuple)
+        fs= RATE
+        R = 512
+        Nfft = 512
+        N=len(input_tuple)
 
 
-	x =stftfun(input_tuple[1:N/2], N/2, Nfft);
+        x =signal.stft(input_tuple,fs,nperseg=Nfft);
 
-	#Set phase to zero in STFT-domain
-	y1 = abs(x)
-	#Synthesize 'robotic' speech signal
-	y1 = istftfun(y1, N/2, N);
-	
-	outputpast= y1/max(abs(y1));
+        #Set phase to zero in STFT-domain
+        X1 = abs(x)
+        #Synthesize 'robotic' speech signal
+        y1 = signal.istft(X1,fs,nperseg=Nfft);
 
-	#SECOND PART OF SIGNAL
-	x2 =stftfun(input_tuple[N/2:N], N, Nfft);
+        y2= y1/max(abs(y1));
 
-	#Set phase to zero in STFT-domain
-	y2 = abs(x2)
-	#Synthesize 'robotic' speech signal
-	y2 = istftfun(y2, R, N);
-	outputnow= y2/max(abs(y2));
-	#OVERLAP
-	outputblock[N/2]=outputpast+outputnow
-
-	#HOW TO SAVE PREVIOUS OUTPUT VALUE?
-	return(outputblock)
+        return(y2)
 def funN2(input_tuple):
 
 	output_block = len(input_tuple) * [0]
 
 	for n in range(0, BLOCKLEN):
 	   
-	    output_block[n] = abs((input_tuple[n]))
+	    output_block[n] =abs((input_tuple[n]))
 	return(output_block)
 
 
 def funN6(input_tuple):
 	#AM Modulation
-	BLOCKLEN= 64
-	output_block = BLOCKLEN * [0]
+	length= len(input_tuple)
+	output_block = length * [0]
 
 	# f0 = 0      # Normal audio
-	f0 = 100   # Modulation frequency (Hz)
+	f0 = 400   # Modulation frequency (Hz)
 	om = 2*math.pi*f0/RATE
 	theta = 0
-	for n in range(0, BLOCKLEN):
+	for n in range(0, length):
 	    # No processing:
         # output_block[n] = input_tuple[n]  
         # OR
@@ -71,7 +61,6 @@ def funN6(input_tuple):
 def funN3(input_block):
 	#block filtering
 	import numpy as np
-	import scipy.signal
 	b0 =  0.008442692929081
 	b2 = -0.016885385858161
 	b4 =  0.008442692929081
@@ -86,7 +75,7 @@ def funN3(input_block):
 	MAXVALUE = (2**15)-1  # Maximum allowed output signal value (because WIDTH = 2)
 	ORDER = 4   # filter is fourth order
 	states = np.zeros(ORDER)
-	[output_block, states] = scipy.signal.lfilter(b, a, input_block, zi = states)
+	[output_block, states] = signal.lfilter(b, a, input_block, zi = states)
 
     # clipping
 	output_block = np.clip(output_block, -MAXVALUE, MAXVALUE)
@@ -94,55 +83,7 @@ def funN3(input_block):
     # convert to integer
 	output_block = output_block.astype(int)
 	return(output_block)
-def funN5(input_tuple):
-	
-	#vibrato
-	from myfunctions import clip16
-	f0 = 2
-	W = 0.2   # W = 0 for no effect
-	BUFFER_LEN =  64         # Set buffer length.
-	buffer = np.zeros(BUFFER_LEN)   # list of zeros
 
-	# Buffer (delay line) indices
-	kr = 0  # read index
-	kw = int(0.5 * BUFFER_LEN)  # write index (initialize to middle of buffer)
-	
-	# Get previous and next buffer values (since kr is fractional)
-	for n in range(0, 64):
-
-
-		kr_prev = int(math.floor(kr))
-		frac = kr - kr_prev    # 0 <= frac < 1
-		kr_next = kr_prev + 1
-		if kr_next == BUFFER_LEN:
-			kr_next = 0
-
-		    # Compute output value using interpolation
-		print(frac)
-		print(buffer[kr_prev])
-		print(buffer[kr_next])
-		print(type(buffer))
-		y0 = (1-frac) * buffer[kr_prev] + frac * buffer[kr_next]
-
-		    # Update buffer
-		buffer[kw] = input_tuple
-
-		    # Increment read index
-		kr = kr + 1 + W * math.sin( 2 * math.pi * f0 * n / RATE )
-		        # Note: kr is fractional (not integer!)
-
-		    # Ensure that 0 <= kr < BUFFER_LEN
-		if kr >= BUFFER_LEN:
-		        # End of buffer. Circle back to front.
-			kr = kr - BUFFER_LEN
-
-		    # Increment write index    
-		kw = kw + 1
-		if kw == BUFFER_LEN:
-		        # End of buffer. Circle back to front.
-			kw = 0
-		output_bytes = struct.pack('h', int(clip16(y0)))
-	return(output_bytes)
 def funN7(input_tuple):
 	#slow voice
 	N=len(input_tuple)
@@ -156,8 +97,32 @@ def funN7(input_tuple):
 			outputblock[n]=0
 	return(outputblock)
 
+def funN5(input_tuple):
+        x0= input_tuple
+        length= len(input_tuple)
+        y0= [0]*length
+
+        # Set parameters of delay system
+        b0 = 1.0    # Gain for direct path
+        G = 0.8
+
+        delay_sec = 0.05 # 50 milliseconds
+        N = int( RATE * delay_sec )   # delay in samples
+
+        BUFFER_LEN = N              # length of buffer
+        buffer = BUFFER_LEN * [0]   # list of zeros
+
+        for i in range(0, length):
 
 
+            y0[i] = b0 * x0[i] + G * buffer[0]
+
+            # Update buffer
+            buffer.append(x0)
+            del buffer[0]       # remove first value
+
+        return(y0)
+gain=1
 root = Tk.Tk()
 gain = Tk.DoubleVar()
 R = 64
@@ -166,8 +131,11 @@ gain.set(0.2 * 2**15)
 BLOCKLEN = R      # Number of frames per block
 WIDTH = 2           # Number of bytes per signal value
 CHANNELS = 1        # mono
-RATE = 8000        # Frame rate (frames/second)
+RATE = 44100        # Frame rate (frames/second)
 RECORD_SECONDS = 5
+
+#creating buttons
+
 def funback1():
 	global effect_num
 	effect_num=2
@@ -187,7 +155,6 @@ def funback7():
 	global effect_num
 	effect_num =7
 
-
 def fun_noeffect():
 	global effect_num
 	effect_num = 0
@@ -196,6 +163,7 @@ def fun_quit():
 	global CONTINUE
 	CONTINUE = False
 
+#start streaming
 
 p = pyaudio.PyAudio()
 
@@ -230,7 +198,6 @@ N1 = Tk.Button(root, text = 'Robot Voice', command = funback1)
 N2 = Tk.Button(root, text = 'absolute value Voice', command = funback2)
 N3 = Tk.Button(root, text = 'Echo Voice', command = funback3)
 N5 = Tk.Button(root, text = 'weird geek', command = funback5)
-
 N6 = Tk.Button(root, text = 'Helium Voice', command = funback6)
 # using higher frequency
 N7 = Tk.Button(root, text = 'Lazy Voice', command = funback7)
@@ -273,16 +240,14 @@ while CONTINUE:
 	input_tuple = struct.unpack('h' * BLOCKLEN, input_bytes)
 	if effect_num == 1:
 		output_block=funN1(input_tuple)
-	#if effect_num == 5:
-	#	output_block=funN5(input_tuple)
+	if effect_num == 5:
+		output_block=funN5(input_tuple)
 	if effect_num == 2:
 		output_block=funN2(input_tuple)
 	if effect_num == 6:
 		output_block=funN6(input_tuple)
 	if effect_num == 3:
 		output_block=funN3(input_tuple)
-	if effect_num == 8:
-		output_block=funN8(input_tuple)
 	if effect_num == 7:
 		output_block = funN7(input_tuple)
 	elif effect_num == 0:
@@ -290,8 +255,8 @@ while CONTINUE:
 	if effect_num == 7:
 		output_bytes = struct.pack('h' * BLOCKLEN*2, *output_block)
 	else:
-		output_bytes = struct.pack('h' * BLOCKLEN, *output_block)
 
+		output_bytes = struct.pack('h' * BLOCKLEN, *output_block)
 	stream.write(output_bytes)
 
 print('* Finished')
